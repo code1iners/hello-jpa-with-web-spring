95a0c75857e2adbb125956147450df0c09138cb3
Codeliner <codeliner96@gmail.com>
2021-05-12 10:35:46 +0900

    Upgrade domain objetcs related to relationship.

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -4,6 +4,14 @@ import lombok.Getter;
 
 import javax.persistence.Embeddable;
 
+/**
+ * <h3>Embeddable type.</h5>
+ * <ul>
+ *     <li>Properties is only have to settable by public or <b>protected</b> constructor.</li>
+ *     <li>Do not create setter methods <b>(for safety)</b>.</li>
+ *     <li><b>Why?</b> it's when JPA implementation library created, able to support many features such as Reflection or Proxy.</li>
+ * </ul>
+ */
 @Embeddable
 @Getter
 public class Address {
@@ -11,4 +19,13 @@ public class Address {
     private String city;
     private String street;
     private String zipcode;
+
+    protected Address() {
+    }
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 }
